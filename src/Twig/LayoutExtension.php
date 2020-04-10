95faa9d4bc081782c8dc36b2b2b647acc70db08d
Philippe Massart <philippe@philmassart.net>
2020-04-10 09:31:07 +0200

    add twig extension -> alcohol and container

--- src/Twig/LayoutExtension.php
+++ src/Twig/LayoutExtension.php
@@ -11,13 +11,26 @@ class LayoutExtension extends AbstractExtension
     {
         return [
             new TwigFilter('price', [$this, 'price'], ["is_safe" => ["html"]]),
-            new TwigFilter('prices', [$this, 'price'], ["is_safe" => ["html"]]),
+            new TwigFilter('alcohol', [$this, 'alcohol'], ["is_safe" => ["html"]]),
+            new TwigFilter('capacity', [$this, 'capacity'], ["is_safe" => ["html"]]),
         ];
     }
 
     public function price($price, $currency = "€")
     {
         if ($price == '') return null;
-        return number_format($price, 2,'.', ' ').' '.$currency;
+        return number_format($price, 2,',', ' ').' '.$currency;
+    }
+
+    public function alcohol($alcohol, $percent = "%")
+    {
+        if ($alcohol == '') return null;
+        return number_format($alcohol, 1,',', ' ').' '.$percent;
+    }
+
+    public function capacity($capacity)
+    {
+        if ($capacity == '') return null;
+        return number_format($capacity, 1,',', ' ');
     }
 }
